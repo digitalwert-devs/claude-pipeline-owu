30263a427c1b652530f6cd9f1bf4a2b4879e6736
Sami Salih <sami.salih@digitalwert.de>
2024-12-10 09:07:57 +0100

    fix: updated models

--- claude-proxy.py
+++ claude-proxy.py
@@ -42,12 +42,19 @@ class Pipeline:
         }
 
     def get_anthropic_models(self):
+        # return [
+        #     {"id": "claude-3-haiku-20240307", "name": "claude-3-haiku"},
+        #     {"id": "claude-3-opus-20240229", "name": "claude-3-opus"},
+        #     {"id": "claude-3-sonnet-20240229", "name": "claude-3-sonnet"},
+        #     {"id": "claude-3-5-sonnet-20241022", "name": "claude-3.5-sonnet"},
+        #     {"id": "claude-3-5-sonnet-20240620", "name": "claude-3.5-sonnet-2024-06-20"},
+        # ]
         return [
             {"id": "claude-3-haiku-20240307", "name": "claude-3-haiku"},
             {"id": "claude-3-opus-20240229", "name": "claude-3-opus"},
             {"id": "claude-3-sonnet-20240229", "name": "claude-3-sonnet"},
+            {"id": "claude-3-5-haiku-20241022", "name": "claude-3.5-haiku"},
             {"id": "claude-3-5-sonnet-20241022", "name": "claude-3.5-sonnet"},
-            {"id": "claude-3-5-sonnet-20240620", "name": "claude-3.5-sonnet-2024-06-20"},
         ]
 
     async def on_startup(self):
