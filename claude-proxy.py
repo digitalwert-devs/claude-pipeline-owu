96ae524b543c5cd4a5dda443ed753c8a91184692
Sami Salih <sami.salih@digitalwert.de>
2024-12-10 09:10:40 +0100

    fix pt. 2: updated model list

--- claude-proxy.py
+++ claude-proxy.py
@@ -50,11 +50,11 @@ class Pipeline:
         #     {"id": "claude-3-5-sonnet-20240620", "name": "claude-3.5-sonnet-2024-06-20"},
         # ]
         return [
-            {"id": "claude-3-haiku-20240307", "name": "claude-3-haiku"},
-            {"id": "claude-3-opus-20240229", "name": "claude-3-opus"},
-            {"id": "claude-3-sonnet-20240229", "name": "claude-3-sonnet"},
-            {"id": "claude-3-5-haiku-20241022", "name": "claude-3.5-haiku"},
-            {"id": "claude-3-5-sonnet-20241022", "name": "claude-3.5-sonnet"},
+            {"id": "claude-3-haiku-latest", "name": "claude-3-haiku"},
+            {"id": "claude-3-opus-latest", "name": "claude-3-opus"},
+            {"id": "claude-3-sonnet-latest", "name": "claude-3-sonnet"},
+            {"id": "claude-3-5-haiku-latest", "name": "claude-3.5-haiku"},
+            {"id": "claude-3-5-sonnet-latest", "name": "claude-3.5-sonnet"},
         ]
 
     async def on_startup(self):
